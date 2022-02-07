68d418013c49886108d161ab1e2d3de2118df3dd
FelipeLopes1996 <felipel.lima1996@gmail.com>
2022-02-07 16:22:40 -0300

    transition in open and close filters bars

--- frontend/src/components/FilterButtons/styles.js
+++ frontend/src/components/FilterButtons/styles.js
@@ -27,7 +27,7 @@ export const WrapperFilters = styled.aside`
     font-weight: 600;
     background-color: ${(props) => props.theme.bg.white};
     box-shadow: 0 0 1rem #ccc;
-    transition: ease-in-out 500ms;
+    transition: all 600ms ease-in-out;
   }
   .hide {
     display: none;
@@ -42,12 +42,21 @@ export const WrapperFilters = styled.aside`
   }
 
   @media (max-width: 768px) {
-    position: absolute;
-    display: ${(props) => (props.Open ? "flex" : "none")};
+    position: fixed;
+    width: ${(props) => (props.Open ? "35%" : "0")};
+    padding: ${(props) => (props.Open ? "1rem" : "0")};
     background-color: ${(props) => props.theme.bg.darkBlue};
     z-index: 2;
-    width: 35%;
     border-radius: 0 9px 9px 0;
+    transition: all 200ms ease-in-out;
+    border: none;
+
+    button {
+      display: ${(props) => !props.Open && "none"};
+    }
+    h1 {
+      display: ${(props) => !props.Open && "none"};
+    }
   }
 `;
 
@@ -55,8 +64,9 @@ export const OpenAsside = styled.button`
   display: none;
 
   @media (max-width: 768px) {
-    position: absolute;
+    position: fixed;
     top: 25rem;
+    display: flex;
     display: ${(props) => (props.Open ? "none" : "flex")};
     align-items: center;
     justify-content: center;
@@ -65,6 +75,7 @@ export const OpenAsside = styled.button`
     border: none;
     background-color: ${(props) => props.theme.bg.darkBlue};
     color: ${(props) => props.theme.bg.white};
+    transition: all 300ms ease-in-out;
     svg {
       color: ${(props) => props.theme.bg.white};
     }
